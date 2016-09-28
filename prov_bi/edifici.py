05406db36f5166f3156b0f166efa6b1398a52cf3
Andrea Musuruane <musuruan@gmail.com>
2016-09-28 17:03:12 +0200

    Added other features.

--- prov_bi/edifici.py
+++ prov_bi/edifici.py
@@ -98,35 +98,106 @@ def filterTags(attrs):
         if tags["name"] == "Carabinieri":
             tags["amenity"] = "police"
             tags["operator"] = "Carabinieri"
+            
+        if tags["name"] == "Questura":
+            tags["amenity"] = "police"
+            tags["operator"] = "Polizia di Stato"
+            
+        if tags["name"] == "Polizia Stradale":
+            tags["amenity"] = "police"
+            tags["operator"] = "Polizia di Stato"
+            
+        if tags["name"] == "Polizia Municipale":
+            tags["amenity"] = "police"
+            tags["operator"] = "Polizia Municipale"
+            
+        if tags["name"] == "Guardia di Finanza":
+            tags["amenity"] = "police"
+            tags["operator"] = "Guardia di Finanza"
+            
+        if tags["name"] == "Ecocentro":
+            tags["amenity"] = "recycling"
+            tags["recycling_type"] = "centre"
         
         if tags["name"] == "Municipio":
             tags["building"] = "civic"
             tags["amenity"] = "townhall"
-        if tags["name"] == "Ufficio Postale":
+            
+        if tags["name"] == "Prefettura":
+            tags["building"] = "public"
+            tags["office"] = "government"
+            
+        if tags["name"]  == "Torre campanaria":
+            tags["man_made"] = "tower"
+            tags["tower:type"] = "bell_tower"
+       
+        if tags["name"].startswith("Ufficio Postale"):
             tags["amenity"] = "post_office"
+            
+        if tags["name"].startswith("Casa di Riposo"):
+            tags["amenity"] = "social_facility"
+            tags["social_facility"] = "assisted_living"
+            tags["social_facility:for"] = "senior"
+            
+        if tags["name"].startswith("Palazzetto dello Sport"):
+            tags["leisure"] = "sports_centre"
+            
+        if tags["name"].startswith("Scuola Materna"):
+            tags["amenity"] = "kindergarten"
+            
+        if tags["name"].startswith("Stazione Ferroviaria"):
+            tags["building"] = "train_station"
+            
+        if tags["name"].startswith("Casa di cura") or tags["name"] == "Ospedale" or  tags["name"] == "Nuovo Ospedale":
+            tags["building"] = "hospital"
+            
+        if tags["name"].startswith("Vigili del Fuoco"):
+            tags["building"] = "fire_station"
         
         firstWord = tags["name"][:tags["name"].find(" ")]
-        
-        if firstWord == "Ambulatorio":
-            tags["amenity"] = "clinic"
-        
-        if firstWord == "Farmacia":
-            tags["amenity"] = "pharmacy"
-            tags["dispensing"] = "yes"
-        
-        if firstWord == "Scuola" or tags["name"] == "Istituto Alberghiero Statale":
-            tags["building"] = "school"
-        
-        if firstWord == "Palestra":
+
+        if firstWord == "Biblioteca":
+            tags["amenity"] = "library"
+            
+        if firstWord == "Bocciodromo":
             tags["leisure"] = "sports_centre"
-         
-        # if firstWord == "Hotel":
-            # tags["building"] = "hotel"
+            tags["sport"] = "boules"
+            tags["type"] = "lyonnaise"
+        
+        if firstWord == "Castello":
+            tags["historic"] = "castle"
+            
+        if firstWord in ("Cinema", "Cineteatro"):
+            tags["amenity"] = "cinema"
+            tags["building"] = "cinema"
             
         if firstWord == "Cimitero":
             tags["tomb"] = "columbarium"
-        
-        if firstWord == "Alpe" or firstWord == "Cascina" or firstWord == "Cascinotto" or firstWord == "Tenuta" or firstWord == "Discarica" or firstWord == "Cimitero":
+            
+        if firstWord == "Diga":
+            tags["waterway"] = "dam"
+            
+        if firstWord in ("Ecomuseo", "Museo"):
+            tags["amenity"] = "museum"
+            
+        if firstWord == "Farmacia":
+            tags["amenity"] = "pharmacy"
+            tags["dispensing"] = "yes"
+            
+        if firstWord == "Palestra":
+            tags["leisure"] = "sports_centre"
+            
+        if firstWord in ("Scuola", "Istituto"):
+            tags["building"] = "school"
+            
+        if firstWord == "Teatro":
+            tags["amenity"] = "theatre"
+            
+        if firstWord == u"Università":
+            tags["building"] = "university"
+            tags["amenity"] = "university"
+            
+        if firstWord == "Alpe" or firstWord == "Cascina" or firstWord == "Cascinotto" or firstWord == "Tenuta" or firstWord == "Discarica" or firstWord == "Cimitero" or tags["name"].startswith("Stazione Ferroviaria"):
             del tags["name"]
         
     return tags
